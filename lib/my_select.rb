544ec14294e0c38b92210b068f6d3b4b406e7b01
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-08 16:48:11 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ arr = Array.new
+ while i < collection.size
+   x = collection[i]
+   yield(x)
+   if yield(x)
+     arr << x
+   end
+   i+= 1
+ end
+ return arr
 end
